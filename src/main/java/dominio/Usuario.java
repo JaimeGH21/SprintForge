12a1ed29467e5e281773b5a5c9ecdaff3f1d69f8
JaimeGH21 <jaime.garcia21@alu.uclm.es>
2026-05-05 20:13:28 +0200

    Añadida clase Usuario

--- src/main/java/dominio/Usuario.java
+++ src/main/java/dominio/Usuario.java
@@ -1,1 +1,12 @@
 
+package dominio;
+
+public class Usuario {
+    private String login;
+    private String pass;
+    private String nombre;
+    private String apellidos;
+    private String direccion;
+    
+    public Usuario() {}
+}
