49c841b38031463ef1b1edf2b0ab1b392b8ff086
JaimeGH21 <jaime.garcia21@alu.uclm.es>
2026-05-05 20:41:27 +0200

    Creada VentanaBusqueda

---
+++ src/main/java/es/uclm/sprintforge/presentation/VentanaBusqueda.java
@@ -0,0 +1,7 @@
+package es.uclm.sprintforge.presentation;
+
+public class VentanaBusqueda {
+    public void buscar() {
+        System.out.println("Buscando alojamientos disponibles...");
+    }
+}
