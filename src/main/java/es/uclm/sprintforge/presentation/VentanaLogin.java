a7fdcc6230be07007bd6bd09b011750c374705ba
JaimeGH21 <jaime.garcia21@alu.uclm.es>
2026-05-05 20:40:30 +0200

    Creada VentanaLogin

---
+++ src/main/java/es/uclm/sprintforge/presentation/VentanaLogin.java
@@ -0,0 +1,7 @@
+package es.uclm.sprintforge.presentation;
+
+public class VentanaLogin {
+    public void mostrar() {
+        System.out.println("Pantalla de inicio de sesión de SprintForge");
+    }
+}
