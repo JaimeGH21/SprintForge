83ff67861c374e423db9464ee98c16b201b8ed2a
JaimeGH21 <jaime.garcia21@alu.uclm.es>
2026-05-05 20:35:11 +0200

    Creado ReservaDAO

---
+++ src/main/java/es/uclm/sprintforge/persistence/ReservaDAO.java
@@ -0,0 +1,8 @@
+package es.uclm.sprintforge.persistence;
+import es.uclm.sprintforge.entity.Reserva;
+
+public class ReservaDAO {
+    public void insertarReserva(Reserva r) {
+        // Código SQL para guardar la reserva
+    }
+}
