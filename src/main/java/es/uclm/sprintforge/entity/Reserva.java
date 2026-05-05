6a24cf0a462db8de2389793f164ed5a45dba555f
JaimeGH21 <jaime.garcia21@alu.uclm.es>
2026-05-05 20:31:01 +0200

    Creada clase Reserva

---
+++ src/main/java/es/uclm/sprintforge/entity/Reserva.java
@@ -0,0 +1,11 @@
+package es.uclm.sprintforge.entity;
+import java.util.Date;
+
+public class Reserva {
+    private Date fechaInicio;
+    private Date fechaFin;
+    private boolean pagado;
+    private boolean confirmada;
+
+    public Reserva() {}
+}
