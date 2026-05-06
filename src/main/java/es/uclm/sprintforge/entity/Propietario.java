dcfae3a0ba320701a6d2327cba72605da16465df
JaimeGH21 <jaime.garcia21@alu.uclm.es>
2026-05-06 18:53:43 +0200

    Creada clase Propietario

--- src/main/java/es/uclm/sprintforge/entity/Propietario.java
+++ src/main/java/es/uclm/sprintforge/entity/Propietario.java
@@ -1,1 +1,20 @@
 
+package es.uclm.sprintforge.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Propietario extends Usuario {
+    private List<Inmueble> propiedades;
+
+    public Propietario(String login, String pass, String nombre, String apellidos, String direccion) {
+        super(login, pass, nombre, apellidos, direccion);
+        this.propiedades = new ArrayList<>();
+    }
+
+    public void añadirPropiedad(Inmueble inmueble) {
+        this.propiedades.add(inmueble);
+    }
+
+    public List<Inmueble> getPropiedades() { return propiedades; }
+}
