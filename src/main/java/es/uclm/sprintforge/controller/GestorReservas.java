f5da3905d4e0878ee2ee92f2274b97a840a3e4d5
JaimeGH21 <jaime.garcia21@alu.uclm.es>
2026-05-06 18:40:44 +0200

    Actualizado el GestorReservas

--- src/main/java/es/uclm/sprintforge/controller/GestorReservas.java
+++ src/main/java/es/uclm/sprintforge/controller/GestorReservas.java
@@ -1,8 +1,32 @@
-package es.uclm.sprintforge.controller;
+package es.uclm.sprintforge.logic;
+
+import es.uclm.sprintforge.entity.Inmueble;
 import es.uclm.sprintforge.entity.Reserva;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GestorReservas {
-    public void confirmarReserva(Reserva r) {
-        // Aquí irá la lógica de confirmación del propietario
+    private List<Inmueble> inmuebles;
+    private List<Reserva> reservas;
+
+    public GestorReservas() {
+        this.inmuebles = new ArrayList<>();
+        this.reservas = new ArrayList<>();
+        // Metemos un par de ejemplos para que no aparezca vacío
+        inmuebles.add(new Inmueble("1", "Calle Altagracia 12", 450.0));
+        inmuebles.add(new Inmueble("2", "Avenida de los Descubrimientos 4", 600.0));
+    }
+
+    public List<Inmueble> getInmuebles() {
+        return inmuebles;
+    }
+
+    public void añadirReserva(Reserva r) {
+        this.reservas.add(r);
+        System.out.println("Reserva añadida con éxito: " + r.getIdReserva());
+    }
+
+    public List<Reserva> getReservas() {
+        return reservas;
     }
 }
