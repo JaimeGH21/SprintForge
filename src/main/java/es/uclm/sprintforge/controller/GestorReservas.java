c69d369c831e52c5e685a8781dbbbf963cd9aa93
JaimeGH21 <jaime.garcia21@alu.uclm.es>
2026-05-06 17:22:37 +0200

    Update GestorReservas.java

--- src/main/java/es/uclm/sprintforge/controller/GestorReservas.java
+++ src/main/java/es/uclm/sprintforge/controller/GestorReservas.java
@@ -4,5 +4,6 @@ import es.uclm.sprintforge.entity.Reserva;
 public class GestorReservas {
     public void confirmarReserva(Reserva r) {
         // Aquí irá la lógica de confirmación del propietario
+        // Comentario para forzar el cambio en la rama
     }
 }
