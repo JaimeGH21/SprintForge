aa0a00b9fce566fb34990b06f40e7dd98025e57e
erikaaamg <erika.macarro@alu.uclm.es>
2026-06-01 17:18:58 +0200

    Create VentanaConfirmacionReservas.java

---
+++ src/main/java/es/uclm/sprintforge/presentacion/VentanaConfirmacionReservas.java
@@ -0,0 +1,8 @@
+package es.uclm.sprintforge.presentacion;
+
+public class VentanaConfirmacionReservas {
+    public void mostrarSolicitudesPendientes() {
+        System.out.println("Pantalla del propietario para aceptar o denegar reservas.");
+    }
+}
+
