03925139e275c2b9ebe794d10a225184d4331b27
erikaaamg <erika.macarro@alu.uclm.es>
2026-06-01 17:05:25 +0200

    Create VentanaLogin.java

---
+++ src/main/java/es/uclm/sprintforge/presentacion/VentanaLogin.java
@@ -0,0 +1,9 @@
+package es.uclm.sprintforge.presentacion; 
+
+public class VentanaLogin {
+    
+    public void mostrar() {
+        System.out.println("Pantalla de inicio de sesión de SprintForge");
+    }
+}
+
