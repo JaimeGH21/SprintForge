0d4d32b8e47dca149d03bd20eebbba66c9ef9126
erikaaamg <erika.macarro@alu.uclm.es>
2026-06-01 17:07:21 +0200

    Creamos el Gestor de Inmuebles

---
+++ src/main/java/es/uclm/sprintforge/negocio/GestorInmuebles.java
@@ -0,0 +1,14 @@
+package es.uclm.sprintforge.negocio;
+
+import es.uclm.sprintforge.dominio.Inmueble;
+import es.uclm.sprintforge.dominio.Propietario;
+
+public class GestorInmuebles {
+
+    public GestorInmuebles() {}
+
+    public void registrarPropiedad(Inmueble inmueble, Propietario propietario) {
+        System.out.println("Registrando inmueble en la dirección: " + inmueble.getDireccion());
+    }
+}
+
