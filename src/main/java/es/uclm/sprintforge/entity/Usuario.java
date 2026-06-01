de811a20763754e86f685a49310962e0324d8b38
erikaaamg <erika.macarro@alu.uclm.es>
2026-06-01 14:43:12 +0200

    Update Usuario.java

--- src/main/java/es/uclm/sprintforge/entity/Usuario.java
+++ src/main/java/es/uclm/sprintforge/entity/Usuario.java
@@ -8,4 +8,47 @@ public class Usuario {
     private String direccion;
 
     public Usuario() {}
+
+    public String getLogin() { 
+        return login; 
+    }
+    public void setLogin(String login) { 
+        this.login = login; 
+    }
+
+    public String getPass() { 
+        return pass; 
+    }
+    public void setPass(String pass) { 
+        this.pass = pass; 
+    }
+
+    public String getNombre() { 
+        return nombre; 
+    }
+    public void setNombre(String nombre) { 
+        this.nombre = nombre; 
+    }
+
+    public String getApellidos() { 
+        return apellidos; 
+    }
+    public void setApellidos(String apellidos) { 
+        this.apellidos = apellidos; 
+    }
+
+    public String getDireccion() { 
+        return direccion; 
+    }
+    public void setDireccion(String direccion) { 
+        this.direccion = direccion; 
+    }
+
+    public String getAttribute() { 
+        return attribute; 
+    }
+    public void setAttribute(String attribute) { 
+        this.attribute = attribute; 
+    }
 }
+
