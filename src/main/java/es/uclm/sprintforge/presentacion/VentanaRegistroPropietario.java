db55daea6afe2b0560256254acb7d1a8af08f6db
erikaaamg <erika.macarro@alu.uclm.es>
2026-06-01 17:15:43 +0200

    Create VentanaRegistroPropietario.java

---
+++ src/main/java/es/uclm/sprintforge/presentacion/VentanaRegistroPropietario.java
@@ -0,0 +1,9 @@
+package es.uclm.sprintforge.presentacion;
+
+public class VentanaRegistroPropietario extends VentanaRegistro {
+    public void mostrarFormularioPropietario() {
+        System.out.println("Formulario de registro para Propietarios.");
+    }
+}
+
+
