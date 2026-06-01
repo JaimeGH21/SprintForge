86e48f747a8185aa192484ee6e138051e772a502
erikaaamg <erika.macarro@alu.uclm.es>
2026-06-01 17:18:00 +0200

    Creamos Alta de Inmuebles para publicar el inmueble

---
+++ src/main/java/es/uclm/sprintforge/presentacion/VentanaAltaInmuebles.java
@@ -0,0 +1,8 @@
+package es.uclm.sprintforge.presentacion;
+
+public class VentanaAltaInmuebles {
+    public void mostrar() {
+        System.out.println("Pantalla para publicar un nuevo inmueble.");
+    }
+}
+
