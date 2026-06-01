1b484ed2ee77e34623bdec3ebef6ccb7ab77bfd4
erikaaamg <erika.macarro@alu.uclm.es>
2026-06-01 16:39:25 +0200

    Delete src/main/java/es/uclm/sprintforge/controller directory

--- src/main/java/es/uclm/sprintforge/controller/GestorReservas.java
+++
@@ -1,8 +0,0 @@
-package es.uclm.sprintforge.controller;
-import es.uclm.sprintforge.entity.Reserva;
-
-public class GestorReservas {
-    public void confirmarReserva(Reserva r) {
-        // Aquí irá la lógica de confirmación del propietario
-    }
-}
