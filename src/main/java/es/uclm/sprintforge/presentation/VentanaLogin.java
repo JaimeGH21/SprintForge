3cd4046c0b3c445af6c895341c0af7f788202069
erikaaamg <erika.macarro@alu.uclm.es>
2026-06-01 17:06:03 +0200

    Delete src/main/java/es/uclm/sprintforge/presentation/VentanaLogin.java

--- src/main/java/es/uclm/sprintforge/presentation/VentanaLogin.java
+++
@@ -1,7 +0,0 @@
-package es.uclm.sprintforge.presentation;
-
-public class VentanaLogin {
-    public void mostrar() {
-        System.out.println("Pantalla de inicio de sesión de SprintForge");
-    }
-}
