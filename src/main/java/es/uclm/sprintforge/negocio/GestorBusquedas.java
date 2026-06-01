b3fb5ff0eaf7206d694c119c81ddaec12cea62e1
erikaaamg <erika.macarro@alu.uclm.es>
2026-06-01 17:07:50 +0200

    Creamos Gestor de Búsquedas

---
+++ src/main/java/es/uclm/sprintforge/negocio/GestorBusquedas.java
@@ -0,0 +1,16 @@
+package es.uclm.sprintforge.negocio;
+
+import es.uclm.sprintforge.dominio.Inmueble;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GestorBusquedas {
+
+    public GestorBusquedas() {}
+
+    public List<Inmueble> buscarAlojamiento(String destino, String fechaInicio, String fechaFin) {
+        System.out.println("Buscando alojamiento en: " + destino);
+        return new ArrayList<>();
+    }
+}
+
