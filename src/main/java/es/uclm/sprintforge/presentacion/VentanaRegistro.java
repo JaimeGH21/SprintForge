75a5ca3632d8049bab90b8e23a3bc91866f08f5e
erikaaamg <erika.macarro@alu.uclm.es>
2026-06-01 17:15:12 +0200

    Creamos la Ventana de Registro para mostrar la interfaz de registro

---
+++ src/main/java/es/uclm/sprintforge/presentacion/VentanaRegistro.java
@@ -0,0 +1,8 @@
+package es.uclm.sprintforge.presentacion;
+
+public class VentanaRegistro {
+    public void mostrar() {
+        System.out.println("Mostrando interfaz base de registro.");
+    }
+}
+
