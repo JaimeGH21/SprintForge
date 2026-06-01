35adbec7876396b64c00ddebeab0626b35126765
erikaaamg <erika.macarro@alu.uclm.es>
2026-06-01 17:16:18 +0200

    Create VentanaRegistroInquilino.java

---
+++ src/main/java/es/uclm/sprintforge/presentacion/VentanaRegistroInquilino.java
@@ -0,0 +1,8 @@
+package es.uclm.sprintforge.presentacion;
+
+public class VentanaRegistroInquilino extends VentanaRegistro {
+    public void mostrarFormularioInquilino() {
+        System.out.println("Formulario de registro para Inquilinos.");
+    }
+}
+
