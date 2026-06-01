e1d6a950e423d739f6e48ff4a97944c7a4dc6da7
erikaaamg <erika.macarro@alu.uclm.es>
2026-06-01 17:18:35 +0200

    Creamos Ventana de Reservas para tramitar nueva reserva

---
+++ src/main/java/es/uclm/sprintforge/presentacion/VentanaReservas.java
@@ -0,0 +1,8 @@
+package es.uclm.sprintforge.presentacion;
+
+public class VentanaReservas {
+    public void mostrarFormularioReserva() {
+        System.out.println("Formulario para tramitar la reserva.");
+    }
+}
+
