eec462e53b9635a6c21e43ec486dac308249bdc6
erikaaamg <erika.macarro@alu.uclm.es>
2026-06-01 17:10:57 +0200

    Creamos el Gestor de los Pagos

---
+++ src/main/java/es/uclm/sprintforge/negocio/GestorPagos.java
@@ -0,0 +1,19 @@
+package es.uclm.sprintforge.negocio;
+
+import es.uclm.sprintforge.dominio.Pago;
+import es.uclm.sprintforge.dominio.Reserva;
+
+public class GestorPagos {
+
+    public GestorPagos() {}
+public void procesarPago(Reserva reserva, Pago pago) {
+        System.out.println("Procesando pago con método: " + pago.getMetodo());
+        reserva.setPagado(true);
+    }
+
+    public void devolverDinero(Reserva reserva) {
+        System.out.println("Devolviendo importe de la reserva rechazada.");
+        reserva.setPagado(false);
+    }
+}
+
